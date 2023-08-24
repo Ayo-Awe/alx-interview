5dc6cf27422b28c4d5ce13643433b136b82f5690
Awe Ayomidipupo <pupoawe@gmail.com>
2023-08-25 00:26:30 +0100

    feat: solve task

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""This module contais code for the making change
+alx interview task
+"""
+
+from typing import List
+
+
+def makeChange(coins: List[int], total: int) -> int:
+    """Returns the least amount of coins required
+    to makeup total
+    """
+
+    if total <= 0:
+        return 0
+
+    sorted_coins = sorted(coins[:], reverse=True)
+
+    min = sorted_coins[-1]
+
+    if (min > total):
+        return 0
+
+    total_coins = 0
+    remainder = total
+
+    for coin in sorted_coins:
+        num_coins = remainder//coin
+        total_coins += num_coins
+        remainder -= num_coins * coin
+
+        if remainder == 0:
+            break
+
+    if remainder != 0:
+        return -1
+
+    return total_coins
